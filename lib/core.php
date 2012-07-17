31bf16b0362a704383aca12eb946fb3319ba9340
Michael Aherne <michael@micaherne.co.uk>
2012-07-17 23:17:09 +0100

    Retain response in hottextInteraction

--- lib/core.php
+++ lib/core.php
@@ -587,10 +587,17 @@ class qti_hottext extends qti_element {
         $result = "<span class=\"qti_hottext\">\n";
         
         $identifier = $this->attrs['identifier'];
-        
         $hottextInteraction = $controller->context['hottextInteraction'];
         
-        $result .= "<input type=\"{$hottextInteraction->hottextType}\" name=\"{$hottextInteraction->variableName}{$hottextInteraction->brackets}\" value=\"{$identifier}\" /> ";
+        $variable = $controller->response[$hottextInteraction->variableName];
+        $testvar = new qti_variable('single', $variable->type, array('value' => $identifier));
+        if ($variable->cardinality == 'multiple') {
+            $comparisonresult = $variable->contains($testvar);
+        } else {
+            $comparisonresult = $variable->match($testvar);
+        }
+        $checked = $comparisonresult->value ? " checked=\"checked\" " : "";
+        $result .= "<input type=\"{$hottextInteraction->hottextType}\" name=\"{$hottextInteraction->variableName}{$hottextInteraction->brackets}\" value=\"{$identifier}\" {$checked}/> ";
         
         foreach($this->children as $child) {
             $result .= $child($controller);
