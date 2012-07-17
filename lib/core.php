8a2763114a98a99fe0d07dc77423535912d91497
Michael Aherne <michael@micaherne.co.uk>
2012-07-17 22:27:51 +0100

    Basic hottextInteraction implementation

--- lib/core.php
+++ lib/core.php
@@ -235,6 +235,7 @@ class qti_gapMatchInteraction extends qti_element{
 }
 
 // TODO: Implement "required" attribute
+// TODO: gap is of type choice therefore should support shuffle and fixed
 class qti_inlineChoiceInteraction extends qti_element {
     
     public $inlineChoice = array();
@@ -370,6 +371,58 @@ class qti_extendedTextInteraction extends qti_stringInteraction {
 
 }
 
+class qti_hottextInteraction extends qti_element {
+
+    /* hottextInteraction doesn't implement the shuffle attribute, even though
+     * the hottext elements are choices (and therefore theoretically support fixed)
+     */
+    public $hottext = array();
+    public $fixed = array(); // indices of hottexts with fixed set to true
+    public $prompt;
+
+    public function __invoke($controller) {
+        $variableName = $this->attrs['responseIdentifier'];
+        $result = "<div id=\"hottextInteraction_{$variableName}\" class=\"qti_blockInteraction\">";
+
+        // Work out what kind of HTML tag will be used for hottexts
+        if (!isset($controller->response[$variableName])) {
+            throw new Exception("Declaration for $variableName not found");
+        }
+
+        $responseVariable = $controller->response[$variableName];
+        $this->hottextType = 'radio';
+        $this->brackets = ''; // we need brackets for multiple responses
+        if ($responseVariable->cardinality == 'multiple') {
+            $this->hottextType = 'checkbox';
+            $this->brackets = '[]';
+        }
+
+        $this->variableName = $variableName; // to be used by embedded hottext elements
+        
+        $controller->context['hottextInteraction'] = $this;
+        $this->displayNodes = array();
+        // Process child nodes just to find hottexts
+        foreach($this->children as $child) {
+            if ($child instanceof qti_prompt) {
+                $this->prompt = $child;
+            } else {
+                $this->displayNodes[] = $child;
+            }
+        }
+
+        if (!is_null($this->prompt)) {
+            $result .= $this->prompt->__invoke($controller);
+        }
+
+        foreach($this->displayNodes as $node) {
+            $result .= $node->__invoke($controller);
+        }
+        
+        $result .= "</div>";
+        return $result;
+    }
+
+}
 class qti_endAttemptInteraction extends qti_element {
 
     public function __invoke($controller) {
@@ -382,8 +435,6 @@ class qti_endAttemptInteraction extends qti_element {
     }
 
 }
-
-
 
 class qti_uploadInteraction extends qti_element {
 
@@ -526,6 +577,28 @@ class qti_gap extends qti_element {
         return $result;
     }
     
+}
+
+class qti_hottext extends qti_element {
+
+    public $interactionType = 'choiceInteraction';
+
+    public function __invoke($controller) {
+        $result = "<span class=\"qti_hottext\">\n";
+        
+        $identifier = $this->attrs['identifier'];
+        
+        $hottextInteraction = $controller->context['hottextInteraction'];
+        
+        $result .= "<input type=\"{$hottextInteraction->hottextType}\" name=\"{$hottextInteraction->variableName}{$hottextInteraction->brackets}\" value=\"{$identifier}\" /> ";
+        
+        foreach($this->children as $child) {
+            $result .= $child($controller);
+        }
+        $result .= "</span>";
+        return $result;
+    }
+
 }
 
 class qti_simpleChoice extends qti_element {
